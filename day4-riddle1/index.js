8cefb41ac3ede26b251714fce8a6eda3e2787690
Arkadiusz Pabian <arkadiusz.pabian@gmail.com>
2022-12-06 23:05:58 +0100

    Added riddle for day 4

---
+++ day4-riddle1/index.js
@@ -0,0 +1,40 @@
+'use strict';
+var fs = require('fs');
+var EOL = require('os').EOL;
+
+var input = fs.readFileSync('./assets/input-random.txt').toString();
+
+function run(input) {
+
+    var lines = input.split(EOL);
+
+    let occurences = 0;
+
+
+    for (const line of lines) {
+        
+        var splittedRanges = line.split(',');
+
+        var splittedRangeA = splittedRanges[0].split('-');
+        var splittedRangeB = splittedRanges[1].split('-');
+
+        var rangeAMin = Number.parseInt(splittedRangeA[0], 10);
+        var rangeAMax = Number.parseInt(splittedRangeA[1], 10);
+
+        var rangeBMin = Number.parseInt(splittedRangeB[0]);
+        var rangeBMax = Number.parseInt(splittedRangeB[1]);
+
+        if (
+            (rangeAMin <= rangeBMin && rangeAMax >= rangeBMax) ||
+            (rangeBMin <= rangeAMin && rangeBMax >= rangeAMax)
+        ) {
+            occurences++;
+        }
+    }
+
+    return occurences;
+}
+
+var result = run(input);
+
+console.log(result);
