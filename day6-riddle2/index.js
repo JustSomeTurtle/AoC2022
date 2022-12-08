57fb3706ec793744ce2ad6bc070b70dd0e073f99
Arkadiusz Pabian <arkadiusz.pabian@gmail.com>
2022-12-08 22:09:47 +0100

    Added riddle for day 6

---
+++ day6-riddle2/index.js
@@ -0,0 +1,26 @@
+var fs = require('fs');
+
+var input = fs.readFileSync('./assets/input-random.txt').toString();
+
+function detectStartOfPacket(input, packetLength) {
+    var bufferedLetters = [];
+    for (let i = 0; i < input.length; i++) {
+        
+        var letter = input[i];
+
+        bufferedLetters.push(letter);
+        
+        if (bufferedLetters.length > packetLength) {
+
+            bufferedLetters.shift();
+
+            if (bufferedLetters.filter((v, i, a) => a.indexOf(v) === i).length === packetLength) {
+                return i + 1;
+            }
+        }
+    }
+}
+
+var result = detectStartOfPacket(input, 14);
+
+console.log(result);
